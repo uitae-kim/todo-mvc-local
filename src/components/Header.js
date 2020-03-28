6f8fbec1807548c7370b19c4d7ca9a4039f43648
minidonut <nycom13@gmail.com>
2020-03-28 17:39:59 +0900

    Add header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,18 @@
 import React from "react";
 import styled from "styled-components";
 
+const StyledHeader = styled.header`
+  > p {
+    color: rgba(175, 47, 47, 0.15);
+    font-weight: 100;
+    font-size: 100px;
+    text-align: center;
+    margin: 16px;
+  }
+`;
+
 export const Header = () => {
-  return <div></div>;
+  return <StyledHeader>
+    <p>todos</p>
+  </StyledHeader>;
 };
