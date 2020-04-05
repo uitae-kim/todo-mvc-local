33a09aa01abaf49205fdc4df0e95823cd0c9e01c
minidonut <nycom13@gmail.com>
2020-04-05 14:48:59 +0900

    Add todo reducer

---
+++ src/redux/reducers/TodoReducer.js
@@ -0,0 +1,39 @@
+import * as types from "../ActionTypes";
+
+const INITIAL_STATE = {
+  todos: [],
+};
+
+let todoCounter = 0;
+
+export const todoReducer = (state = INITIAL_STATE, { type, payload }) => {
+  switch (type) {
+    case types.ADD_TODO:
+      return {
+        todos: [
+          ...state.todos,
+          { id: todoCounter++, content: payload.content, done: false },
+        ]
+      };
+    case types.UPDATE_TODO:
+      return {
+        todos: state.todos.map(todo => {
+          if (todo.id === payload.id) {
+            return {
+              id: todo.id,
+              content: payload.content || todo.content,
+              done: payload.done || todo.done,
+            };
+          } else {
+            return todo;
+          }
+        }),
+      };
+    case types.DELETE_TODO:
+      return {
+        todos: state.todos.filter(x => x.id !== payload.id),
+      };
+    default:
+      return state;
+  };
+};
